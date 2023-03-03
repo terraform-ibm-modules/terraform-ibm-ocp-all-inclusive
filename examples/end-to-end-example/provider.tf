704d813c80770c6c797b16140c0224325d49acd8
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2023-03-03 12:39:46 +0000

    Update provider.tf

--- examples/end-to-end-example/provider.tf
+++ examples/end-to-end-example/provider.tf
@@ -23,20 +23,19 @@ provider "kubernetes" {
   host  = data.ibm_container_cluster_config.cluster_config.host
   token = data.ibm_container_cluster_config.cluster_config.token
 }
-
-# Retrieve IAM access token (required for restapi provider)
-data "ibm_iam_auth_token" "token_data" {
+    
+locals {
+  at_endpoint = "https://api.${var.region}.logging.cloud.ibm.com"
 }
 
-# restapi provider required by terraform-ibm-key-protect-all-inclusive module
-provider "restapi" {
-  uri                   = "https:"
-  write_returns_object  = false
-  create_returns_object = false
-  debug                 = false
-  headers = {
-    Authorization    = data.ibm_iam_auth_token.token_data.iam_access_token
-    Bluemix-Instance = module.key_protect_all_inclusive.key_protect_guid
-    Content-Type     = "application/vnd.ibm.kms.policy+json"
-  }
+provider "logdna" {
+  alias      = "at"
+  servicekey = module.observability_instances.activity_tracker_resource_key != null ? module.observability_instances.activity_tracker_resource_key : ""
+  url        = local.at_endpoint
+}
+
+provider "logdna" {
+  alias      = "ld"
+  servicekey = module.observability_instances.logdna_resource_key != null ? module.observability_instances.logdna_resource_key : ""
+  url        = local.at_endpoint
 }
