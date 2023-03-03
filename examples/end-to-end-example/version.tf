89f732947181d9e28371d86e5e5f5da25226b606
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2023-03-03 12:41:04 +0000

    Update version.tf

--- examples/end-to-end-example/version.tf
+++ examples/end-to-end-example/version.tf
@@ -12,6 +12,11 @@ terraform {
     kubernetes = {
       version = "2.16.1"
     }
+    # The logdna provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
+    logdna = {
+      source  = "logdna/logdna"
+      version = ">= 1.14.2"
+    }
     # The restapi provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
     restapi = {
       source  = "Mastercard/restapi"
