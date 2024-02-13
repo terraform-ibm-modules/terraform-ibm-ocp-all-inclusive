f92e8437ae51c7f9670437c9823edce7b9f58670
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2024-02-13 13:24:11 +0000

    fix: versions

--- examples/end-to-end-example/version.tf
+++ examples/end-to-end-example/version.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.3.0, <1.6.0"
+  required_version = ">= 1.3.0, <1.62.0"
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.6.0"
+      version = ">= 1.62.0"
     }
     helm = {
       source  = "hashicorp/helm"
