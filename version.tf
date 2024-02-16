c9d7d39ebb0411b7bb1941a51bc07afc88d420ff
Md Anam Raihan <Md.Anam.Raihan@ibm.com>
2024-02-16 16:12:29 +0000

    chore: update version constraints

--- version.tf
+++ version.tf
@@ -9,32 +9,32 @@ terraform {
     # tflint-ignore: terraform_unused_required_providers
     external = {
       source  = "hashicorp/external"
-      version = ">= 2.2.3"
+      version = ">= 2.2.3, < 3.0.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.8.0"
+      version = ">= 2.8.0, < 3.0.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.16.1"
+      version = ">= 2.16.1, < 3.0.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     local = {
       source  = "hashicorp/local"
-      version = ">= 2.2.3"
+      version = ">= 2.2.3, < 3.0.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.1"
+      version = ">= 3.2.1, < 4.0.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.9.1"
+      version = ">= 0.9.1, < 1.0.0"
     }
   }
 }
