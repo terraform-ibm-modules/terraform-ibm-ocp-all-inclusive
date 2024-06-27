d9b8d209c6a3160ca30e8e0350492bb50f0c90e3
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2024-06-27 12:34:38 +0530

    added validation block

--- variables.tf
+++ variables.tf
@@ -289,6 +289,10 @@ variable "operating_system" {
   type        = string
   description = "The operating system of the workers in the default worker pool. If no value is specified, the current default version OS will be used. See https://cloud.ibm.com/docs/openshift?topic=openshift-openshift_versions#openshift_versions_available ."
   default     = null
+  validation {
+    condition     = var.operating_system == null || var.operating_system == "RHEL 8" || var.operating_system == "RHCOS"
+    error_message = "RHEL8 or Red Hat Enterprise Linux CoreOS (RHCOS) are the allowed OS values. RHCOS requires VPC clusters created from 4.15 onwards. Upgraded clusters from 4.14 cannot use RHCOS."
+  }
 }
 
 ##############################################################################
