1ef4e126071eadeebee963778a2b76f1ac501c00
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-07-28 00:09:42 +0100

    fix(deps): update terraform terraform-ibm-modules/observability-agents/ibm to v1.28.3 (#302)

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ module "ocp_base" {
 module "observability_agents" {
   count                                    = var.log_analysis_enabled == true || var.cloud_monitoring_enabled == true ? 1 : 0
   source                                   = "terraform-ibm-modules/observability-agents/ibm"
-  version                                  = "1.28.2"
+  version                                  = "1.28.3"
   cluster_id                               = module.ocp_base.cluster_id
   cluster_resource_group_id                = var.resource_group_id
   cluster_config_endpoint_type             = var.cluster_config_endpoint_type
