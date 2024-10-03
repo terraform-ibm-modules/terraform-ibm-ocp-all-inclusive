d240581cd95143bd87be5f00ba6a3d5e00e9c0ea
prateek <sharma_prateek@outlook.com>
2024-10-03 22:02:49 +0530

    feat: added support for ocp v4.16  (#330)

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -88,7 +88,7 @@ func TestRunCompleteExample(t *testing.T) {
 	t.Parallel()
 
 	// This test should always test the latest and the earliest supported OCP versions.
-	versions := []string{"4.15"}
+	versions := []string{"4.16"}
 	for _, version := range versions {
 		t.Run(version, func(t *testing.T) { testRunComplete(t, version) })
 	}
@@ -99,7 +99,7 @@ func TestRunUpgradeCompleteExample(t *testing.T) {
 
 	terraformVars := map[string]interface{}{
 		// This test should always test the OCP version not tested in the "TestRunCompleteExample" test.
-		"ocp_version": "4.14",
+		"ocp_version": "4.15",
 	}
 	options := setupOptions(t, "ocp-all-upg", terraformVars)
 
--- variables.tf
+++ variables.tf
@@ -58,6 +58,7 @@ variable "ocp_version" {
       var.ocp_version == "4.13",
       var.ocp_version == "4.14",
       var.ocp_version == "4.15",
+      var.ocp_version == "4.16",
     ])
     error_message = "The specified ocp_version is not one of the validated versions."
   }
