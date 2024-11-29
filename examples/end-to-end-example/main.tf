7d4d68258a9728d73d3f74518097fbdbc8f47f2a
Akash Kumar <akash.kumar@ibm.com>
2024-11-29 12:38:33 +0530

    kp-private

--- examples/end-to-end-example/main.tf
+++ examples/end-to-end-example/main.tf
@@ -213,12 +213,15 @@ locals {
 }
 
 module "key_protect_all_inclusive" {
-  source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "4.16.11"
-  resource_group_id         = module.resource_group.resource_group_id
-  region                    = var.region
-  key_protect_instance_name = "${var.prefix}-kp"
-  resource_tags             = var.resource_tags
+  source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
+  version                     = "4.16.11"
+  resource_group_id           = module.resource_group.resource_group_id
+  region                      = var.region
+  key_protect_instance_name   = "${var.prefix}-kp"
+  resource_tags               = var.resource_tags
+  key_protect_allowed_network = "private-only"
+  key_ring_endpoint_type      = "private"
+  key_endpoint_type           = "private"
   keys = [
     {
       key_ring_name = (local.key_ring_name)
