64e7c2871747d45480e9616e7166903ee40b09de
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-22 21:07:40 +0000

    fix(deps): update terraform-module

--- examples/private-only-example/main.tf
+++ examples/private-only-example/main.tf
@@ -191,7 +191,7 @@ module "cbr_zone_schematics" {
 
 module "observability_instances" {
   source                         = "terraform-ibm-modules/observability-instances/ibm"
-  version                        = "3.4.3"
+  version                        = "3.5.0"
   region                         = var.region
   resource_group_id              = module.resource_group.resource_group_id
   cloud_logs_instance_name       = "${var.prefix}-icl"
@@ -213,7 +213,7 @@ locals {
 
 module "key_protect_all_inclusive" {
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "4.21.2"
+  version                     = "4.21.3"
   resource_group_id           = module.resource_group.resource_group_id
   region                      = var.region
   key_protect_instance_name   = "${var.prefix}-kp"
