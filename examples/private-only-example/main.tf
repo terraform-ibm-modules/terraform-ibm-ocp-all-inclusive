52425832659896a4fce5ca4b5ce7e2a81f7cc297
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-22 08:42:00 +0000

    fix(deps): update terraform-module

--- examples/private-only-example/main.tf
+++ examples/private-only-example/main.tf
@@ -191,7 +191,7 @@ module "cbr_zone_schematics" {
 
 module "observability_instances" {
   source                         = "terraform-ibm-modules/observability-instances/ibm"
-  version                        = "3.4.3"
+  version                        = "3.5.0"
   region                         = var.region
   resource_group_id              = module.resource_group.resource_group_id
   cloud_logs_instance_name       = "${var.prefix}-icl"
