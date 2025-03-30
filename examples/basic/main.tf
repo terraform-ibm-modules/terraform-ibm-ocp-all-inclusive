7cb0c0b03695d14165bbd2d30fb543420819cd44
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-30 21:14:03 +0000

    fix(deps): update terraform-module

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -105,7 +105,7 @@ locals {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.20.2"
+  version           = "7.22.2"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
@@ -176,7 +176,7 @@ locals {
 
 module "key_protect_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "4.21.3"
+  version                   = "4.21.4"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
   key_protect_instance_name = "${var.prefix}-kp"
